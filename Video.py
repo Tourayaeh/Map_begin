51d0199106982514422b73a9381074091476d72d
Tourayaeh <46813328+Tourayaeh@users.noreply.github.com>
2019-01-30 16:39:10 +0100

    Create Video.py

---
+++ Video.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+def extract_image_one_fps(video_source_path):
+    
+    vidcap = cv2.VideoCapture(video_source_path)
+    count = 0
+    success = True
+    while success:
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))
+        success,image = vidcap.read()
+        
+        ## Stop when last frame is identified
+        image_last = cv2.imread("frame{}.png".format(count-1))
+        if np.array_equal(image,image_last):
+            break
+                
+        cv2.imwrite("frame%d.png" % count, image)     # save frame as PNG file
+        print ('{}.sec reading a new frame: {} '.format(count,success))
+        count += 1
+
+
+extract_image_one_fps('rally.mp4')
